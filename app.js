caca317e48cc39dde23533ca26ce24d76efe18f9
unknown <aishwaryababu1212@gmail.com>
2020-08-20 11:24:38 +0530

    added start script

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-//var analyse = require('./analyse-tweet');
+var analyse = require('./analyse-tweet');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
